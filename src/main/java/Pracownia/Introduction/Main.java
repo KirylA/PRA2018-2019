a5d0a14324250a4cec7e86c2fdf15132f4ff1d17
kiryl <kirave@st.amu.edu.pl>
2018-10-06 20:11:37 +0300

    finalCode

--- src/main/java/Pracownia/Introduction/Main.java
+++ src/main/java/Pracownia/Introduction/Main.java
@@ -1,8 +1,12 @@
 package Pracownia.Introduction;
 
-public class Main {
+import java.util.logging.Logger;
 
+public class Main {
+    static Logger logger = Logger.getLogger("name");
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+
+        logger.info("message");
     }
 }
